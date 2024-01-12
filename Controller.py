d3d7dde37eb33033c48d611f7c7db14faa28efb1
Philipp Kremling <philipp.kremling.pk@gmail.com>
2024-01-12 21:55:28 +0100

    Initial commit

---
+++ Controller.py
@@ -0,0 +1,195 @@
+import io
+import openai
+from PIL import Image
+from io import BytesIO
+from flask import Flask, request, jsonify, send_file
+import uuid
+import base64
+from threading import Thread
+import logging
+
+from StatusCodes import StatusCodes
+from Service import call_openai_vision
+from Transactions import transactions
+from Messages import Messages
+
+app = Flask(__name__)
+
+apiVersion = "v1"
+contextRoot = f"/api/{apiVersion}"
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+last_transaction_id = None
+
+
+@app.route(f'{contextRoot}/healthcheck', methods=['GET'])
+def health_check():
+    return jsonify({"status": "up"}), 200
+
+
+@app.route(f'{contextRoot}/upload', methods=['POST'])
+def upload_image():
+    return handle_uploaded_image(False)
+
+
+@app.route(f'{contextRoot}/upload-dall-e', methods=['POST'])
+def upload_image_dall_e():
+    return handle_uploaded_image(True)
+
+
+def handle_uploaded_image(use_dall_e):
+    global last_transaction_id
+    logging.info('Received upload request')
+    if 'inputFile' not in request.files:
+        logging.debug('Cancelling upload request because of no input file sent')
+        return jsonify({"message": Messages.NO_INPUT_FILE_SENT}), 400
+
+    input_file = request.files['inputFile']
+
+    if input_file.filename == '':
+        logging.debug('Cancelling upload request because of no input file found')
+        return jsonify({"message": Messages.INPUT_FILE_NOT_FOUND}), 404
+
+    # Convert image to PIL
+    image = Image.open(input_file.stream)
+
+    image_format = image.format
+    if not image_format:
+        logging.debug('Cancelling upload request because no format could be determined')
+        return jsonify({"message": Messages.UNABLE_DETERMINING_IMAGE_FORMAT}), 400
+
+    image_format = image_format.lower()
+    if image_format not in ['png', 'jpeg', 'gif', 'webp']:
+        logging.warning('Image format was manually casted to JPEG')
+        image_format = 'jpeg'
+    else:
+        image_format = image.format
+
+    # Convert PI to Bytes object
+    buffered = BytesIO()
+    image.save(buffered, format=image_format)
+    img_byte = buffered.getvalue()
+
+    if len(img_byte) > 20 * 1024 * 1024:
+        logging.debug('Cancelling upload request because image file exceeds 20MB')
+        return jsonify({"message": Messages.FILE_SIZE_EXCEEDING}), 413
+
+    # Convert Bytes object to image encoded with Base64
+    img_base64 = base64.b64encode(img_byte).decode('utf-8')
+
+    transaction_id = str(uuid.uuid4())
+    last_transaction_id = transaction_id
+    transactions[transaction_id] = {
+        "status": StatusCodes.RECEIVED.value,
+        "image": img_base64,
+        "analysis": None,
+        "score": None,
+        "error": None
+    }
+
+    # Start async task
+    thread = Thread(target=async_call_openai_vision, args=(transaction_id, use_dall_e))
+    thread.start()
+
+    return jsonify({"message": Messages.IMAGE_RECEIVED, "transaction_id": transaction_id}), 200
+
+
+# Wrapper function for asynchronous execution
+def async_call_openai_vision(transaction_id, fast_processing):
+    try:
+        call_openai_vision(transaction_id, fast_processing)
+        logging.info('✅ Generation process done')
+    except openai.BadRequestError as e:
+        logging.error(f'Caught BadRequestError while calling OpenAI Vision: {e}')
+        transactions[transaction_id]["status"] = StatusCodes.ERROR.value
+        transactions[transaction_id]["error"] = Messages.OPENAI_DENIAL
+    except Exception as e:
+        logging.error(f'Caught error while calling OpenAI Vision: {e}')
+        transactions[transaction_id]["status"] = StatusCodes.ERROR.value
+        transactions[transaction_id]["error"] = Messages.ERROR_INPUT_FILE
+
+
+@app.route(f'{contextRoot}/analysis/<transaction_id>', methods=['GET'])
+def get_analysis(transaction_id):
+    logging.info('Received analysis request')
+    if transaction_id not in transactions:
+        logging.debug(f'Cancelling analysis request because no transaction with id {transaction_id} was found')
+        return jsonify({"error": Messages.TRANSACTION_NOT_FOUND_OR_INVALID}), 404
+
+    status = transactions[transaction_id]["status"]
+    if status == StatusCodes.ERROR.value:
+        logging.debug('Cancelling analysis request because transaction is in error')
+        return jsonify({"message": Messages.TRANSACTION_IN_ERROR}), 409
+    if status == StatusCodes.RUNNING_ANALYSIS.value or status == StatusCodes.RECEIVED.value:
+        logging.debug('Cancelling analysis request because transaction has no content')
+        return jsonify({"message": Messages.NO_CONTENT}), 204
+
+    return jsonify({"transaction_id": transaction_id, "analysis": transactions[transaction_id]["analysis"]}), 200
+
+
+@app.route(f'{contextRoot}/score/<transaction_id>', methods=['GET'])
+def get_score(transaction_id):
+    logging.info('Received score request')
+    if transaction_id not in transactions:
+        logging.debug(f'Cancelling score request because no transaction with id {transaction_id} was found')
+        return jsonify({"error": Messages.TRANSACTION_NOT_FOUND_OR_INVALID}), 404
+
+    status = transactions[transaction_id]["status"]
+    if status == StatusCodes.ERROR.value:
+        logging.debug('Cancelling score request because transaction is in error')
+        return jsonify({"message": Messages.TRANSACTION_IN_ERROR}), 409
+    if (status == StatusCodes.RUNNING_ANALYSIS.value or
+            status == StatusCodes.RUNNING_GENERATION.value or
+            status == StatusCodes.RECEIVED.value or
+            status == StatusCodes.IDLING.value):
+        logging.debug('Cancelling score request because transaction has no content')
+        return jsonify({"message": Messages.NO_CONTENT}), 204
+
+    base64_encoded_image = transactions[transaction_id]["score"]
+    image_data = base64.b64decode(base64_encoded_image)
+    img_byte_arr = io.BytesIO(image_data)
+
+    return send_file(
+        img_byte_arr,
+        mimetype='image/jpeg',
+        as_attachment=True,
+        download_name='score.jpg'
+    ), 200
+
+
+@app.route(f'{contextRoot}/last-score', methods=['GET'])
+def get_last_request():
+    if last_transaction_id is None or last_transaction_id not in transactions:
+        return jsonify({"message": Messages.NO_LAST_TRANSACTION_FOUND}), 404
+
+    return get_score(last_transaction_id)
+
+
+@app.route(f'{contextRoot}/status/<transaction_id>', methods=['GET'])
+def get_status(transaction_id):
+    logging.info('Received status request')
+    if transaction_id not in transactions:
+        logging.debug(f'Cancelling status request because no transaction with id {transaction_id} was found')
+        return jsonify({"error": Messages.TRANSACTION_NOT_FOUND_OR_INVALID}), 404
+
+    return jsonify({"transaction_id": transaction_id, "status": transactions[transaction_id]["status"]}), 200
+
+
+@app.route(f'{contextRoot}/error/<transaction_id>', methods=['GET'])
+def get_error(transaction_id):
+    logging.info('Received error request')
+    if transaction_id not in transactions:
+        logging.debug(f'Cancelling error request because no transaction with id {transaction_id} was found')
+        return jsonify({"error": Messages.TRANSACTION_NOT_FOUND_OR_INVALID}), 404
+
+    status = transactions[transaction_id]["status"]
+    if status != StatusCodes.ERROR:
+        logging.debug('Cancelling error request because transaction is not in error')
+        return jsonify({"error": Messages.NO_ERRORS}), 409
+
+    return jsonify({"transaction_id": transaction_id, "error": transactions[transaction_id]["error"]}), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4242)
---
+++ Service.py
@@ -0,0 +1,223 @@
+import base64
+import configparser
+from io import BytesIO
+
+import requests
+from torchvision.transforms import ToTensor
+
+from PIL import Image
+from openai import OpenAI
+from StatusCodes import StatusCodes
+from Transactions import transactions
+from Messages import Messages
+import logging
+from diffusers import StableDiffusionImg2ImgPipeline
+from compel import Compel
+import torch
+import io
+
+
+def get_api_key():
+    """
+    Reads the API key from the config file
+    """
+    config = configparser.ConfigParser()
+    with open("secure/openAI.properties") as stream:
+        config.read_string("[top]\n" + stream.read())
+    return config.get('top', 'openai.key')
+
+
+api_key = get_api_key()
+open_ai_client = OpenAI(api_key=api_key)
+
+sd_model = "/nfs/scratch/students/kremlingph95027/content/v1-5-pruned-emaonly.safetensors"
+lora_folder = "/nfs/scratch/students/kremlingph95027/stable-diffusion-webui/models/Lora"
+lora_weights_1_name = "v15Tune_Lora_17800.safetensors"
+lora_weights_2_name = "15Tune_Lora_17100.safetensors"
+lora_weights_3_name = "v15Tune_Lora_12800.safetensors"
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def call_openai_vision(transaction_id, use_dall_e):
+    """
+    Calls OpenAI Vision to run an image analysis
+
+    Args:
+        :param transaction_id: The ID of the transaction for which the analysis should be executed
+        :param use_dall_e: Bool flag indicating whether SD (False) or DALL-E (True) should be used
+    """
+    transactions[transaction_id]["status"] = StatusCodes.RUNNING_ANALYSIS.value
+    image = transactions[transaction_id]["image"]
+
+    logging.info('Calling OpenAI Vision')
+
+    response = open_ai_client.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": """Als musikalische Bilderkennung
+    analysiere dieses Bild auf Basis seiner visuellen Eigenschaften, die für die Generierung
+    einer musikalischen Partitur relevant sein könnten
+    wobei du die gefundenen Eigenschaften in einer komma-getrennten Liste herausschreibst.
+    Achte auf verschiedene Farben und interpretiere diese als eigene Musiker.
+    Achte besonders auf folgende Eigenschaften und baue sie falls passend ein: abstrakt, chaotisch, duester, freundlich,
+    geschwungen, gleichmaessig, hoch, minimalistisch, tief. Beschränke dich jedoch nicht auf diese Eigenschaften.
+    Gib das Ergebnis in folgendem Format aus: 'Farbe Form (Position): Eigenschaften'. 
+    #Beispiel: 'Gelbe Linie (oben rechts): hoch'"""
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{image}"
+                        }
+                    },
+                ],
+            }
+        ],
+        max_tokens=300,
+    )
+
+    logging.debug('OpenAI Vision Request successful')
+    """
+    Schwarzer Hintergrund
+Grüne Linien (Mitte): chaotisch, geschwungen, freundlich
+Blaue Linie/Gekritzel (unten Mitte): chaotisch, abstrakt
+Lila Streifen (mittig): gleichmäßig, düster
+Blaue Flecken (überall): abstrakt, minimalistisch
+Gelber Blitz (oben rechts): hoch
+    """
+    transactions[transaction_id]["analysis"] = response.choices[0].message.content
+    transactions[transaction_id]["status"] = StatusCodes.IDLING.value
+
+    try:
+        if use_dall_e:
+            call_dall_e(transaction_id)
+        else:
+            call_stable_diffusion(transaction_id)
+    except Exception as e:
+        logging.error(f'Caught error while calling SD: {e}')
+        transactions[transaction_id]["status"] = StatusCodes.ERROR.value
+        transactions[transaction_id]["error"] = Messages.OUTPUT_FILE_ERROR
+
+
+def call_stable_diffusion(transaction_id):
+    """
+    Calls the local Stable Diffusion model to run an image generation
+
+    Args:
+        transaction_id (str): The ID of the transaction for which the generation should be executed
+    """
+    transactions[transaction_id]["status"] = StatusCodes.RUNNING_GENERATION.value
+    analysis = transactions[transaction_id]["analysis"]
+
+    logging.info('Calling SD')
+
+    pipe = StableDiffusionImg2ImgPipeline.from_single_file(
+        sd_model, torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    )
+    pipe.to("cuda")
+    pipe.enable_model_cpu_offload()
+    pipe.enable_xformers_memory_efficient_attention()
+
+    #pipe.load_lora_weights(lora_folder, weight_name=lora_weights_1_name, adapter_name=lora_weights_1_name)
+    #pipe.load_lora_weights(lora_folder, weight_name=lora_weights_2_name, adapter_name=lora_weights_2_name)
+    #pipe.load_lora_weights(lora_folder, weight_name=lora_weights_3_name, adapter_name=lora_weights_3_name)
+
+    #pipe.set_adapters([lora_weights_1_name, lora_weights_2_name, lora_weights_3_name], adapter_weights=[1.0, 1.0, 1.0])
+
+    prompt = f"""
+        Schwarzer Hintergrund
+        {analysis}
+        """
+
+    compel = Compel(tokenizer=pipe.tokenizer,
+                    text_encoder=pipe.text_encoder)
+    prompt_embeds = compel(prompt)
+
+    image = transactions[transaction_id]["image"]
+    img_bytes = base64.b64decode(image)
+    img_buffer = BytesIO(img_bytes)
+    image = Image.open(img_buffer)
+    transform = ToTensor()
+    tensor_image = transform(image)
+
+    image = pipe(
+        prompt_embeds=prompt_embeds,
+        image=tensor_image,
+        strength=0.75,
+        guidance_scale=7.5,
+#        cross_attention_kwargs={"scale": 1.0}
+    ).images[0]
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='JPEG')
+    img_byte_arr.seek(0)
+    base64_encoded_image = base64.b64encode(img_byte_arr.getvalue()).decode('utf-8')
+
+    transactions[transaction_id]["score"] = base64_encoded_image
+    transactions[transaction_id]["status"] = StatusCodes.SUCCESS.value
+
+    logging.debug('SD Request successful')
+
+
+def call_dall_e(transaction_id):
+    """
+    Calls DALL-E for image generation
+
+    Args:
+        transaction_id (str): The ID of the transaction for which the generation should be executed
+    """
+    transactions[transaction_id]["status"] = StatusCodes.RUNNING_GENERATION.value
+    analysis = transactions[transaction_id]["analysis"]
+
+    logging.info('Calling DALL-E')
+
+    prompt = f"""
+            Schwarzer Hintergrund
+            {analysis}
+            """
+
+    response = open_ai_client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    image_url = response.data[0].url
+
+    logging.info(f"Got answer form DALL-E with url {image_url}")
+
+    store_image(image_url, transaction_id)
+
+    logging.debug('DALL-E Request successful')
+
+
+def store_image(url, transaction_id):
+    """
+    Stores a remote image in transactions with size of 512x512
+
+    Args:
+        url (str): The URL where the picture is located
+        transaction_id (str): The ID of the transaction
+    """
+    response = requests.get(url)
+    if response.status_code == 200:
+        image_bytes = io.BytesIO(response.content)
+        image = Image.open(image_bytes)
+        resized_image = image.resize((512, 512))
+        buffered = io.BytesIO()
+        resized_image.save(buffered, format="JPEG")
+        base64_encoded_image = base64.b64encode(buffered.getvalue()).decode()
+
+        transactions[transaction_id]["score"] = base64_encoded_image
+        transactions[transaction_id]["status"] = StatusCodes.SUCCESS.value
+    else:
+        logging.error("Error while downloading image")
+        transactions[transaction_id]["error"] = Messages.ERROR_DOWNLOADING_FILE
+        transactions[transaction_id]["status"] = StatusCodes.ERROR.value
